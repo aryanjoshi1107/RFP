7ada034c54647017beff6bf6c0c86fb05aabc317
Aryan joshi <joshiaryan1107@gmail.com>
2025-11-12 18:31:54 +0530

    ui fixed

--- RFP_Project/RFP_Project/urls.py
+++ RFP_Project/RFP_Project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from users.views import MyLoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',MyLoginView.as_view(),name='Login'),
     path('user/',include('users.urls')),
 ]
--- RFP_Project/users/urls.py
+++ RFP_Project/users/urls.py
@@ -4,7 +4,6 @@ from users.views import Register,MyLoginView,Dashboard
 
 urlpatterns = [
     path('register/',Register.as_view(), name='register'),
-    path('login/',MyLoginView.as_view(),name='Login'),
     path('dashboard/',Dashboard.as_view(), name='Dashboard'),
     path('vendor/register/',Register.as_view(), name='vendor-register')
 ]
